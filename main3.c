8bc2593ccedc0d890e857977031f3901920ab7f8
root <root@localhost.localdomain>
2019-04-07 21:45:00 +0300

    added comment's

--- main3.c
+++ main3.c
@@ -6,14 +6,14 @@
 int _EXFUN(printf, (const char *__restrict, ...)
 _ATTRIBUTE ((__format__ (__printf__, 1, 2))));
 
-typedef struct dlist
+typedef struct dlist //Structure information
 {
 	int data;
 	struct dlist *right;
 	struct dlist *down;
 } Matrix;
 
-Matrix *mInit(int data)
+Matrix *mInit(int data)//Initialization of Structure  --  Matrix 
 {
 	Matrix *temp = (Matrix*)malloc(sizeof(Matrix));
 	
@@ -24,54 +24,61 @@ Matrix *mInit(int data)
 	return(temp);
 }
 
-Matrix addelemRight(int data, Matrix *head)
+Matrix addelemRight(int data, Matrix *head) //Adding element to Right
 {
-	Matrix *temp, *r; 
+	Matrix *temp, *r; //init temp elem
 	temp = (Matrix*)malloc(sizeof(Matrix));
 	
+	printf("5\n");
+	//Setting data
 	temp->data = data;
 	temp->right = NULL;
 	temp->down = NULL;
 	
+	printf("6\n");
+	//Adding elem to Matrix
 	r = head;
+	
 	while (r->right != NULL)
-	{r = r -> right;}
+	{r = r -> right;printf("7\n");}
 	r -> right = temp;
 }
 
-Matrix addelemDown(int data, Matrix *head)
+Matrix addelemDown(int data, Matrix *head) //Adding element to Down
 {
-	Matrix *temp, *d; 
+	Matrix *temp, *d; //init temp elem
 	temp = (Matrix*)malloc(sizeof(Matrix));
 	
+	//Setting data
 	temp -> data = data;
 	temp -> right = NULL;
 	temp -> down = NULL;
 	
-	d = head;
+	//Adding elem to Matrix
+	d = head; 
 	while (d->down != NULL)
 	{d = d -> down;}
 	d -> down = temp;
 }
 
-void mPrint(Matrix *lst, int Row)
-{
+void mPrint(Matrix *lst, int Row) //Outputs All Matrix Elements
+{								  //Prints full row, then step lower
 	Matrix *r, *d;
 	d = lst;
 	int i;
-	for(i =0; i <Row; i++)
+	for(i =0; i <Row; i++) //By row count
 	{
-		for (r = d; r != NULL; r = r->right)
+		for (r = d; r != NULL; r = r->right)//Out full row
 		{
-			printf("%d ", r->data);
-			printf("1\n");
+			printf("%d", r->data);
+			//printf("1\n");
 		}
-		d = d->down;
+		d = d->down;//Step lower
 		if (d != NULL)
 		{
 			r = d;
 		}
-		printf("row done\n");
+		printf("row done\n");//debug
 	}
 }
 
@@ -80,10 +87,29 @@ void main()
 	system("@cls||clear"); // Clear the console
 	Matrix MatrixMain; //Create the Matrix struct
 	//int Elem = 0;
-	mInit(5);
-	addelemRight(5, &MatrixMain);
-	addelemDown(5, &MatrixMain);
-	addelemDown(5, &MatrixMain);
 	
-	mPrint(&MatrixMain, 2);
+	mInit(1);
+	
+	//printf("%d\n", &MatrixMain.right->right->right);
+	
+	/*
+	printf("1\n");//debug
+	addelemRight(1, &MatrixMain);
+	printf("2\n");//debug
+	addelemRight(1, &MatrixMain);
+	addelemRight(1, &MatrixMain); 
+	
+	addelemDown(1, &MatrixMain);
+	addelemDown(1, &MatrixMain);
+	*/
+	
+	/* - Matrix view - *\
+	
+		 |1 1 1 1|
+		 |1 |
+		 |1 |
+		 | |
+	
+	\*                 */
+	//mPrint(&MatrixMain, 1);
 }
